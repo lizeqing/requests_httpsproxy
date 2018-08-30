ca5521b0f50e92e6cff8f0863022fc56cea3ba06
zeqing <lizeqing@datartisan.com>
2018-08-30 10:29:40 +0800

    missing SSL_KEYWORDS

--- requests_httpsproxy/__init__.py
+++ requests_httpsproxy/__init__.py
@@ -14,7 +14,7 @@ from requests.packages.urllib3.connection import (HTTPConnection,
 from requests.packages.urllib3.connectionpool import (HTTPConnectionPool,
                                                       HTTPSConnectionPool)
 from requests.packages.urllib3.exceptions import ConnectTimeoutError, NewConnectionError
-from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.poolmanager import PoolManager, SSL_KEYWORDS
 from requests.packages.urllib3.util.url import parse_url
 from requests.packages.urllib3.packages.ssl_match_hostname import match_hostname
 
