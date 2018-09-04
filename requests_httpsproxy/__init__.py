7edf20df7853003d24d66a5b45a307106187fc76
Living42 <qzeqing@gmail.com>
2018-09-05 03:55:29 +0800

    rewrite

--- requests_httpsproxy/__init__.py
+++ requests_httpsproxy/__init__.py
@@ -4,24 +4,181 @@ This module add HTTPS proxies support to requests.
 
 The HTTPS implementation supports the full range of requests features.
 """
-from __future__ import absolute_import
-
+import ssl
+import logging
 from socket import error as SocketError, timeout as SocketTimeout
-from socket import create_connection as create_socket_connection
+from functools import partial
 
-from requests.packages.urllib3.connection import (HTTPConnection,
-                                                  HTTPSConnection)
-from requests.packages.urllib3.connectionpool import (HTTPConnectionPool,
-                                                      HTTPSConnectionPool)
-from requests.packages.urllib3.exceptions import ConnectTimeoutError, NewConnectionError
-from requests.packages.urllib3.poolmanager import PoolManager, SSL_KEYWORDS
-from requests.packages.urllib3.util.url import parse_url
-from requests.packages.urllib3.packages.ssl_match_hostname import match_hostname
-
-import base64
-import functools
 import tlslite
-import OpenSSL.crypto
+from urllib3.poolmanager import ProxyManager as BaseProxyManager
+from urllib3.connection import HTTPSConnection as BaseHTTPSConnection
+from urllib3.connectionpool import (
+    HTTPConnectionPool as BaseHTTPConnectionPool,
+    HTTPSConnectionPool as BaseHTTPSConnectionPool
+)
+from urllib3.exceptions import (
+    ConnectTimeoutError,
+    NewConnectionError,
+)
+import OpenSSL
+
+log = logging.getLogger(__name__)
+
+
+class ProxyManager(BaseProxyManager):
+    def __init__(self, proxy_url, num_pools=10, headers=None,
+                 proxy_headers=None, **connection_pool_kw):
+        assert proxy_url.startswith('https'), 'only support https proxy'
+
+        super(ProxyManager, self).__init__(
+            proxy_url, num_pools, headers, proxy_headers, **connection_pool_kw)
+
+        self.pool_classes_by_scheme = {
+            'http': HTTPConnectionPool,
+            'https': HTTPSConnectionPool,
+        }
+
+    def connection_from_host(self, host, port=None, scheme='http', pool_kwargs=None):
+        return super(BaseProxyManager, self).connection_from_host(
+            host, port, scheme, pool_kwargs=pool_kwargs)
+
+
+class HTTPConnectionPool(BaseHTTPConnectionPool):
+    ConnectionCls = BaseHTTPSConnection
+
+    def _new_conn(self):
+        """
+        Return a fresh :class:`httplib.HTTPSConnection`.
+        """
+        self.num_connections += 1
+        log.debug("Starting new HTTPS connection (%d): %s",
+                  self.num_connections, self.host)
+
+        actual_host = self.proxy.host
+        actual_port = self.proxy.port
+
+        conn = self.ConnectionCls(host=actual_host, port=actual_port,
+                                  timeout=self.timeout.connect_timeout,
+                                  strict=self.strict, **self.conn_kw)
+
+        return conn
+
+
+class HTTPSConnection(BaseHTTPSConnection):
+    def set_https_tunnel(self, host, port, headers=None):
+        self._tunnel_host = host
+        self._tunnel_port = port
+        self._tunnel_headers = headers
+
+    def connect(self):
+        conn = self._new_conn()
+        self._origin_sock = conn
+
+        if self.ssl_context is None:
+            self.ssl_context = ssl.create_default_context()
+
+        self.sock = self.ssl_context.wrap_socket(
+            conn, server_hostname=self.host)
+
+        self._setup_https_tunnel()
+
+        self.is_verified = (
+            self.ssl_context.verify_mode == ssl.CERT_REQUIRED or
+            self.assert_fingerprint is not None
+        )
+
+    def _setup_https_tunnel(self):
+        sock = self.sock
+
+        host = self._tunnel_host
+        port = self._tunnel_port
+
+        try:
+            lines = []
+            lines.append(b'CONNECT %s:%d HTTP/1.1' % (host, port))
+            lines.append(b'Host: %s:%d' % (host, port))
+
+            if self._tunnel_headers:
+                for item in self._tunnel_headers.items():
+                    lines.append(b'%s: %s' % item)
+
+            sock.sendall(b'\r\n'.join(lines) + b'\r\n\r\n')
+
+            data = b''
+            code = 0
+            pos = -1
+            while True:
+                s = sock.recv(4096)
+                if not s:
+                    if code == 0:
+                        raise SocketError("Tunnel connection failed: %r" % data)
+                    break
+                data += s
+                if code == 0 and b'\r\n' in data:
+                    version, code, message = data.split(b' ', 2)
+                    if code != b'200':
+                        sock.close()
+                        raise SocketError("Tunnel connection failed: %s %s" %
+                                          (code, message.strip()))
+                pos = data.find(b'\r\n\r\n')
+                if pos > 0:
+                    break
+
+            tls_conn = tlslite.TLSConnection(sock)
+            try:
+                tls_conn.handshakeClientCert(serverName=host)
+            except Exception:
+                sock.close()
+                raise
+
+            try:
+                ssl.match_hostname(tlslite_getpeercert(tls_conn), host)
+            except Exception:
+                tls_conn.close()
+                raise
+        except SocketTimeout as e:
+            raise ConnectTimeoutError(
+                self, "Connection to %s timed out. (connect timeout=%s)" %
+                (self.host, self.timeout))
+
+        except SocketError as e:
+            raise NewConnectionError(
+                self, "Failed to establish a new connection: %s" % e)
+
+        # patch fileno,
+        # let urllib3.util.connection.is_connection_dropped work as expected
+        tls_conn.fileno = partial(self._origin_sock.fileno)
+        # patch getpeercert
+        tls_conn.getpeercert = partial(tlslite_getpeercert, tls_conn)
+        self.sock = tls_conn
+
+
+class HTTPSConnectionPool(BaseHTTPSConnectionPool):
+    ConnectionCls = HTTPSConnection
+
+    def _new_conn(self):
+        """
+        Return a fresh :class:`httplib.HTTPSConnection`.
+        """
+        self.num_connections += 1
+        log.debug("Starting new HTTPS connection (%d): %s",
+                  self.num_connections, self.host)
+
+        actual_host = self.proxy.host
+        actual_port = self.proxy.port
+
+        conn = self.ConnectionCls(host=actual_host, port=actual_port,
+                                  timeout=self.timeout.connect_timeout,
+                                  strict=self.strict, **self.conn_kw)
+
+        return self._prepare_conn(conn)
+
+    def _prepare_conn(self, conn):
+        conn.set_cert(assert_hostname=self.proxy.host)
+        return conn
+
+    def _prepare_proxy(self, conn):
+        conn.set_https_tunnel(self.host, self.port, self.proxy_headers)
 
 
 def tlslite_getpeercert(conn):
@@ -54,207 +211,35 @@ def tlslite_getpeercert(conn):
     return conn._peercert
 
 
-class HTTPSProxyConnection(HTTPConnection):
-    """
-    A plain-text HTTP connection that connects via a HTTPSProxy proxy.
-    """
-
-    def __init__(self, *args, **kwargs):
-        self._ssl_options = kwargs.pop('_ssl_options')
-        super(HTTPSProxyConnection, self).__init__(*args, **kwargs)
-
-    def _new_conn(self):
-        """
-        Establish a new connection via the HTTPSProxy proxy.
-        """
-        extra_kw = {}
-        if self.source_address:
-            extra_kw['source_address'] = self.source_address
-
-        try:
-            proxy_host = self._ssl_options['proxy_host']
-            proxy_port = self._ssl_options['proxy_port']
-            username = self._ssl_options.get('username', '')
-            password = self._ssl_options.get('password', '')
-            proxy_port = self._ssl_options['proxy_port']
-            proxy_sock = create_socket_connection(
-                (proxy_host, proxy_port), timeout=self.timeout, **extra_kw)
-            proxy_conn = tlslite.TLSConnection(proxy_sock)
-            proxy_conn.handshakeClientCert(serverName=proxy_host)
-            try:
-                match_hostname(tlslite_getpeercert(proxy_conn), proxy_host)
-            except Exception:
-                proxy_conn.close()
-                raise
-            data = 'CONNECT %s:%d HTTP/1.1\r\n' % (self.host, self.port)
-            if username and password:
-                data += 'Proxy-Authorization: Basic %s\r\n' % base64.b64encode(
-                    username + ':' + password)
-            data += 'Host: %s:%d\r\n' % (self.host, self.port)
-            data += '\r\n'
-            proxy_conn.sendall(data.encode())
-
-            data = b''
-            code = 0
-            pos = -1
-            while True:
-                s = proxy_conn.recv(4096)
-                if not s:
-                    if code == 0:
-                        raise SocketError(
-                            "Tunnel connection failed: %r" % data)
-                    break
-                data += s
-                if code == 0 and b'\r\n' in data:
-                    version, code, message = data.split(b' ', 2)
-                    if code != b'200':
-                        proxy_conn.close()
-                        raise SocketError("Tunnel connection failed: %s %s" %
-                                          (code, message.strip()))
-                pos = data.find(b'\r\n\r\n')
-                if pos > 0:
-                    break
-            proxy_conn.unread(data[pos + 4:])
-            conn = proxy_conn
-        except SocketTimeout as e:
-            raise ConnectTimeoutError(
-                self, "Connection to %s timed out. (connect timeout=%s)" %
-                (self.host, self.timeout))
-
-        except SocketError as e:
-            raise NewConnectionError(
-                self, "Failed to establish a new connection: %s" % e)
-
-        return conn
+PATCHED = False
 
 
-# We don't need to duplicate the Verified/Unverified distinction from
-# urllib3/connection.py here because the HTTPSConnection will already have been
-# correctly set to either the Verified or Unverified form by that module. This
-# means the HTTPSProxyHTTPSConnection will automatically be the correct type.
-class HTTPSProxyHTTPSConnection(HTTPSProxyConnection, HTTPSConnection):
-    pass
+def patch_requests():
+    global PATCHED
+    if PATCHED:
+        raise RuntimeError('patch multiple time')
 
-
-class HTTPSProxyHTTPConnectionPool(HTTPConnectionPool):
-    ConnectionCls = HTTPSProxyConnection
-
-
-class HTTPSProxyHTTPSConnectionPool(HTTPSConnectionPool):
-    ConnectionCls = HTTPSProxyHTTPSConnection
-
-
-class HTTPSProxyProxyManager(PoolManager):
-    """
-    A version of the urllib3 ProxyManager that routes connections via the
-    defined HTTPSProxy proxy.
-    """
-    pool_classes_by_scheme = {
-        'http': HTTPSProxyHTTPConnectionPool,
-        'https': HTTPSProxyHTTPSConnectionPool,
-    }
-
-    def __init__(self,
-                 proxy_url,
-                 username=None,
-                 password=None,
-                 num_pools=10,
-                 headers=None,
-                 **connection_pool_kw):
-        parsed = parse_url(proxy_url)
-
-        self.proxy_url = proxy_url
-
-        self._ssl_options = {
-            # 'ssl_version': ssl_version,
-            'proxy_host': parsed.host,
-            'proxy_port': parsed.port or 443,
-            'username': username,
-            'password': password,
-        }
-
-        super(HTTPSProxyProxyManager, self).__init__(num_pools, headers,
-                                                     **connection_pool_kw)
-
-        self.pool_classes_by_scheme = HTTPSProxyProxyManager.pool_classes_by_scheme
-
-    def _new_pool(self, scheme, host, port, request_context=None):
-        """
-        Create a new :class:`ConnectionPool` based on host, port, scheme, and
-        any additional pool keyword arguments.
-
-        If ``request_context`` is provided, it is provided as keyword arguments
-        to the pool class used. This method is used to actually create the
-        connection pools handed out by :meth:`connection_from_url` and
-        companion methods. It is intended to be overridden for customization.
-        """
-        pool_cls = self.pool_classes_by_scheme[scheme]
-        if request_context is None:
-            request_context = self.connection_pool_kw.copy()
-
-        # Although the context has everything necessary to create the pool,
-        # this function has historically only used the scheme, host, and port
-        # in the positional args. When an API change is acceptable these can
-        # be removed.
-        for key in ('scheme', 'host', 'port'):
-            request_context.pop(key, None)
-
-        if scheme == 'http':
-            for kw in SSL_KEYWORDS:
-                request_context.pop(kw, None)
-
-        # pass _ssl_options here
-        request_context['_ssl_options'] = self._ssl_options
-        return pool_cls(host, port, **request_context)
-
-
-def inject_into_requests():
-    'Monkey-patch requests with HTTPS-Proxy Support.'
     import requests
-    from requests.utils import get_auth_from_url
-    __real_proxy_manager_for = requests.adapters.HTTPAdapter.proxy_manager_for
+
+    origin_proxy_manager_for = requests.adapters.HTTPAdapter.proxy_manager_for
 
     def proxy_manager_for(self, proxy, **proxy_kwargs):
-        if proxy.lower().startswith('https'):
-            username, password = get_auth_from_url(proxy)
-            if proxy not in self.proxy_manager:
-                self.proxy_manager[proxy] = HTTPSProxyProxyManager(
-                    proxy,
-                    username=username,
-                    password=password,
-                    num_pools=self._pool_connections,
-                    maxsize=self._pool_maxsize,
-                    block=self._pool_block,
-                    **proxy_kwargs)
+        if proxy in self.proxy_manager:
             return self.proxy_manager[proxy]
+        elif proxy.startswith('https'):
+            proxy_headers = self.proxy_headers(proxy)
+            manager = self.proxy_manager[proxy] = ProxyManager(
+                proxy,
+                proxy_headers=proxy_headers,
+                num_pools=self._pool_connections,
+                maxsize=self._pool_maxsize,
+                block=self._pool_block,
+                **proxy_kwargs
+            )
+            return manager
         else:
-            return __real_proxy_manager_for(self, proxy, **proxy_kwargs)
+            return origin_proxy_manager_for(self, proxy, **proxy_kwargs)
 
     requests.adapters.HTTPAdapter.proxy_manager_for = proxy_manager_for
-    __real_is_connection_dropped = requests.packages.urllib3.util.connection.is_connection_dropped
 
-    def is_connection_dropped(conn):
-        if isinstance(conn.sock, tlslite.tlsconnection.TLSConnection):
-            sock = conn.sock
-            while isinstance(sock, tlslite.tlsconnection.TLSConnection):
-                sock = sock.sock.socket
-            conn = type('fake_tlslite_conn_type', (object, ), {})()
-            conn.sock = sock
-        return __real_is_connection_dropped(conn)
-
-    requests.packages.urllib3.connectionpool.is_connection_dropped = is_connection_dropped
-    __real_ssl_wrap_socket = requests.packages.urllib3.util.ssl_.ssl_wrap_socket
-
-    def ssl_wrap_socket(sock, **kwargs):
-        if not isinstance(sock, tlslite.tlsconnection.TLSConnection):
-            return __real_ssl_wrap_socket(sock, **kwargs)
-        conn = tlslite.TLSConnection(sock)
-        conn.handshakeClientCert(serverName=kwargs.get('server_hostname'))
-        conn.getpeercert = functools.partial(tlslite_getpeercert, conn)
-        return conn
-
-    requests.packages.urllib3.util.ssl_.ssl_wrap_socket = ssl_wrap_socket
-    requests.packages.urllib3.connection.ssl_wrap_socket = ssl_wrap_socket
-
-
-inject_into_requests()
+    PATCHED = True
